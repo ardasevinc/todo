06242979f9942e95eb08dcf28bed05b06e2e0380
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-04-16 19:20:48 +0300

    feat: header element

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/navbar';
+
+const Header = () => {
+  return (
+    <header className='text-center'>
+      <h1 className='font-bold text-2xl'>TODO</h1>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
