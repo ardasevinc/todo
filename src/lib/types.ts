f23b3a45c13c97b92bca6ed7ecd67563f5c198f5
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-04-16 20:04:09 +0300

    feat: todo type & faker util

---
+++ src/lib/types.ts
@@ -0,0 +1,8 @@
+export type Todo = {
+  title: string;
+  description: string;
+  completed: boolean;
+  created: Date;
+  hidden: boolean;
+  id: string;
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,9 @@ import { twMerge } from 'tailwind-merge';
 
 import { customAlphabet } from 'nanoid';
 
+import { faker } from '@faker-js/faker';
+import type { Todo } from '@/lib/types';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -11,3 +14,17 @@ export const nanoid = customAlphabet(
   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
   7,
 ); // 7-character random string
+
+// generates *n* fake todos using method
+export const fakeTodoArray = (n: number) => {
+  const method = (): Todo => ({
+    title: faker.git.commitMessage(),
+    description: faker.hacker.phrase(),
+    completed: faker.datatype.boolean({ probability: 0.25 }),
+    created: faker.date.recent({ days: 30 }),
+    hidden: faker.datatype.boolean({ probability: 0.1 }),
+    id: nanoid(),
+  });
+
+  return Array.from({ length: n }, method);
+};
